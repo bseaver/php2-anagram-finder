265606654b417d3a21c4a334db72ebdab426db2b
Benjamin T. Seaver and Leah Sherrell <benjamin@>
2017-02-14 14:44:00 -0800

    pass tests so far

--- src/Anagram.php
+++ src/Anagram.php
@@ -3,7 +3,24 @@
     {
         function anagramFinder ($word, $possible_anagrams)
         {
-            return array();
+            $result = array();
+
+            if (!$word || count($possible_anagrams) == 0) {
+                return $result;
+            }
+
+            if (count($possible_anagrams) == 1) {
+                if ($word !== $possible_anagrams[0]) {
+                    $wordls = str_split($word);
+                    $possls = str_split($possible_anagrams[0]);
+                    $inter = array_intersect($wordls, $possls);
+                    if ($inter === $wordls) {
+                        return $possible_anagrams;
+                    }
+                }
+            }
+
+            return $result;
         }
     }
 
