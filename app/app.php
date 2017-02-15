f85c2a0901c1cc397f71f6a9a7b90e2430b149c2
Benjamin T. Seaver and Leah Sherrell <benjamin@>
2017-02-14 16:51:30 -0800

    create twig template and controller for it

--- app/app.php
+++ app/app.php
@@ -13,11 +13,29 @@
     );
 
     $app->get('/', function() use ($app) {
+        $target_word_key = "target_word";
+        $possible_anagrams_key = "possible_anagrams";
+        $actual_anagrams = "";
+        $target_word = "";
+        $actual_anagrams = "";
+        $possible_anagrams = "";
+
+        if (array_key_exists($target_word_key, $_GET) && array_key_exists($possible_anagrams_key, $_GET)) {
+            $target_word = $_GET["target_word"];
+            $possible_anagrams = $_GET["possible_anagrams"];
+            $possible_anagrams_array = explode(" ", $possible_anagrams);
+            $my_anagram = new Anagram;
+            $actual_result = $my_anagram->anagramFinder($target_word, $possible_anagrams_array);
+            $actual_anagrams = implode(" ", $actual_result);
+        }
 
 
         return $app['twig']->render(
             'anagram.html.twig',
             array(
+                "actual_anagrams" => $actual_anagrams,
+                "target_word" => $target_word,
+                "possible_anagrams" => $possible_anagrams
             )
         );
     });
